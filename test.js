8325593e0a1775a2fab5234da97eef9153484f19
madhusudan <86766004+hero1601@users.noreply.github.com>
2024-11-16 15:23:58 -0800

    fixed indent issues

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-let age = 18;
+/* eslint-disable */
+const age = 18
 if (age >= 18) {
-    console.log('ad');
-    console.log('check');
+    console.log('ad')
+    console.log('check')
 } else {
     console.log('test')
 }
