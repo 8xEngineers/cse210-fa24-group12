4d219740b0be0a5d50f1e1b0641460ce52e042f5
BZhang-ucsd <97600878+BZhang-ucsd@users.noreply.github.com>
2024-11-19 18:57:10 -0800

    Update runTest.ts as dummy test

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -6,14 +6,16 @@ async function main() {
 	try {
 		// The folder containing the Extension Manifest package.json
 		// Passed to `--extensionDevelopmentPath`
-		const extensionDevelopmentPath = path.join(__dirname, '../../');
+		const extensionDevelopmentPath = path.resolve(__dirname, '');
 
-		// The path to test runner
-		// Passed to --extensionTestsPath
-		const extensionTestsPath = path.join(__dirname, './suite/index');
-
-		// Download VS Code, unzip it and run the integration test
-		await runTests({ extensionDevelopmentPath, extensionTestsPath });
+	        // The path to the extension test script
+	        const extensionTestsPath = path.resolve(__dirname, '');
+	
+	        // Testint github work flow. No test to run for now.
+	        console.log('Setup check complete. No tests to run.');
+	        console.log(`Development Path: ${extensionDevelopmentPath}`);
+	        console.log(`Tests Path: ${extensionTestsPath}`);
+		// dummyt test
 	} catch (err) {
 		console.error('Failed to run tests');
 		process.exit(1);
