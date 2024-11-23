a761c53fbf980b3f7d15d0c9de17e848e3de8627
madhusudan <86766004+hero1601@users.noreply.github.com>
2024-11-22 19:15:44 -0800

    added rules for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,4 +10,18 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  { rules: {
+    "no-cond-assign": "warn",
+    "no-var": "warn",
+    "prefer-const": "warn",
+    "no-useless-catch": "warn",
+    "no-useless-escape": "warn",
+
+    "@typescript-eslint/no-array-constructor": "warn",
+    "@typescript-eslint/no-empty-object-type": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-unsafe-function-type": "warn",
+    "@typescript-eslint/no-wrapper-object-types": "warn"
+  }}
 ];
