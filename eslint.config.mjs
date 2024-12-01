29ecd043930459284f378be1476030e02e39db66
madhusudan <86766004+hero1601@users.noreply.github.com>
2024-11-30 23:03:17 -0800

    fixed eslint workflow

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,12 +16,14 @@ export default [
     "prefer-const": "warn",
     "no-useless-catch": "warn",
     "no-useless-escape": "warn",
+    "no-undef": "warn",
 
     "@typescript-eslint/no-array-constructor": "warn",
     "@typescript-eslint/no-empty-object-type": "warn",
     "@typescript-eslint/no-explicit-any": "warn",
     "@typescript-eslint/no-unused-vars": "warn",
     "@typescript-eslint/no-unsafe-function-type": "warn",
-    "@typescript-eslint/no-wrapper-object-types": "warn"
+    "@typescript-eslint/no-wrapper-object-types": "warn",
+    "@typescript-eslint/no-require-imports": "warn"
   }}
 ];
