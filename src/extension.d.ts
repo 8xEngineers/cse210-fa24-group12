c79c423d9ab57d57ced9a5154f640bbbaafb5eb1
code123jsr <akashsaranathan@gmail.com>
2024-12-11 15:00:01 -0800

    removed warnings in extension file

--- src/extension.d.ts
+++ src/extension.d.ts
@@ -1,7 +1,7 @@
 import * as vscode from 'vscode';
 import * as J from './';
-export declare var journalStartup: J.Extension.Startup;
-export declare var journalConfiguration: J.Extension.Configuration;
+export declare let journalStartup: J.Extension.Startup;
+export declare let journalConfiguration: J.Extension.Configuration;
 export declare function activate(context: vscode.ExtensionContext): {
     getJournalConfiguration(): J.Extension.Configuration;
 };
